6833641f74162cf5db1b28100c40715a2a1bd8c2
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2024-01-21 09:12:54 +0100

    Update jBase.dev.js

--- jBase.dev.js
+++ jBase.dev.js
@@ -4,116 +4,115 @@
 * Link to Work: https://github.com/k37z3r/jBase
 * Link to Creator Profile: https://github.com/k37z3r
 */
-let _toggleSlideClicked=false;
+let _jBaseTSC=false;
 class JBase extends Array{
-    constructor(element) {
+    constructor(e) {
         super();
-        if (element == document || element instanceof HTMLElement || element == window)
-            this.push(element);
+        if (e == document || e instanceof HTMLElement || e == window)
+            this.push(e);
         else {
-            let elements=document.querySelectorAll(element);
+            let elements=document.querySelectorAll(e);
             for(let i=0;i<elements.length;i++){
                 this.push(elements[i]);
             }
         }
     }
-    ready(callback){
+    ready(_cb){
         this[0].addEventListener('readystatechange', e => {
             if(this[0].readyState === "complete"){
-                callback();
+                _cb();
                 return true;
             }
           });
     }
-    each(callback) {
-        if (callback && typeof(callback) == 'function') {
+    each(_cb) {
+        if (_cb && typeof(_cb) == 'function') {
             for (let i = 0; i < this.length; i++) {
-                callback(this[i], i);
+                _cb(this[i], i);
             }
             return this;
         } 
     }
-    addClass(className) {
-        this.each(function(element) {
-            element.classList.add(className);
+    addClass(arg) {
+        this.each(function(e) {
+            e.classList.add(arg);
         })
         return this;
     }
 
-    removeClass(className) {
-        this.each(function(element) {
-            element.classList.remove(className);
+    removeClass(arg) {
+        this.each(function(e) {
+            e.classList.remove(arg);
         })
         return this;
     }
     val(){
         return this[0].value;
     }
-    hasClass(className) {
-        return this[0].classList.contains(className);
+    hasClass(arg) {
+        return this[0].classList.contains(arg);
     }
-
-    css(propertyObject) {
-        this.each(function(element) {
-            Object.assign(element.style,propertyObject);
+    css(arg) {
+        this.each(function(e) {
+            Object.assign(e.style, arg);
         })
         return this;
     }
-    attr(attr, value = null) {
-        let getattr = this;
-        if (value) {
-            this.each(function(element) {
-                element.setAttribute(attr, value);
+    attr(att, val = null) {
+        let getatt = this;
+        if (val) {
+            this.each(function(e) {
+                e.setAttribute(att, val);
 
             });
         }
         else
-            getattr = this[0].getAttribute(attr);
-        return getattr;
+            getatt = this[0].getAttribute(att);
+        return getatt;
     }
-    removeAttr(attr) {
-        this[0].removeAttribute(attr);
+    removeAttr(arg) {
+        this[0].removeAttribute(arg);
     }
-    html(data) {
-        if (data) {
-            this.each(function(element) {
-                element.innerHTML = data;
+    html(arg) {
+        if (arg) {
+            this.each(function(e) {
+                e.innerHTML = arg;
             })
         }
         else
             return this[0].innerHTML;
         return this;
     }
-    append(element){
-        this[0].append(element);
+    append(arg){
+        this[0].append(arg);
         return this;
     }
-    toggleSlide(data = {}){
+    toggleSlide(arg = {}){
         let timers, transitions, transformIn, transformOut;
-        if (Object.hasOwn(data, 'timer')){
-            if (data.timer == 'slow')
+        if (Object.hasOwn(arg, 'timer')){
+            if (arg.timer == 'slow')
                 timers = '800ms';
-            else if (data.timer == 'fast')
+            else if (arg.timer == 'fast')
                 timers = '200ms';
-            else if (data.timer == 'moderate')
+            else if (arg.timer == 'moderate')
                 timers = '500ms';
             else
-                timers = data.timer;
+                timers = arg.timer;
         }
         else
             timers = '500ms';
-        if (Object.hasOwn(data, 'transition')){
-            if (data.transition == 'ease')
+        if (Object.hasOwn(arg, 'transition')){
+            if (arg.transition == 'ease')
                 transitions = 'ease';
             else
                 transitions = 'linear';
         }
         else
             transitions = 'linear';
-        if (Object.hasOwn(data, 'transform')){
-            if (Object.hasOwn(data.transform, 'in') && Object.hasOwn(data.transform, 'out')){
-                transformIn = data.transform.in
-                transformOut = data.transform.out
+        if (Object.hasOwn(arg, 'transform')){
+            if (Object.hasOwn(arg.transform, 'in') && Object.hasOwn(arg.transform, 'out')){
+                transformIn = arg.transform.in
+                transformOut = arg.transform.out
             }
             else{
                 transformIn = 'translateY(0%)';
@@ -124,84 +123,83 @@ class JBase extends Array{
             transformIn = 'translateY(0%)';
             transformOut = 'translateY(100%)';
         }
-        this.each(function(element) {
-            if (_toggleSlideClicked){
-                _toggleSlideClicked=false;
-                element.style.transform = transformOut;
-                element.style.transitionTimingFunction = transitions;
-                element.style.transition = timers;
+        this.each(function(e) {
+            if (_jBaseTSC){
+                _jBaseTSC=false;
+                e.style.transform = transformOut;
+                e.style.transitionTimingFunction = transitions;
+                e.style.transition = timers;
             }
             else{
-                _toggleSlideClicked=true;
-                element.style.transform = transformIn;
-                element.style.transitionTimingFunction = transitions;
-                element.style.transition = timers;
+                _jBaseTSC=true;
+                e.style.transform = transformIn;
+                e.style.transitionTimingFunction = transitions;
+                e.style.transition = timers;
             }
         });
         return this;
     }
-    prepend(element){
-        this[0].prepend(element);
+    prepend(arg){
+        this[0].prepend(arg);
         return this;
     }
     hide() {
-        this.each(function(element) {
-            element.style.display = "none";
+        this.each(function(e) {
+            e.style.display = "none";
         });
         return this;
     }
     show() {
-        this.each(function(element) {
-            element.style.display = "block";
+        this.each(function(e) {
+            e.style.display = "block";
         });
         return this;
     }
-    on(events, child, callback = null, state = null) {
-        let array = events.split(",");
-        if (callback != null) {
-            let selector = child;
-            this.each(function(element) {
-                array.forEach((event) => {
+    on(events, child, _cb = null, state = null) {
+        let arr = events.split(",");
+        if (_cb != null) {
+            this.each(function(e) {
+                arr.forEach((event) => {
                     event = event.trim();
-                    element.addEventListener(event, function(event) {
-                        if (event.target.matches(selector + ', ' + selector + ' *')) {
-                            callback.apply(event.target.closest(selector), arguments);
+                    e.addEventListener(event, function(event) {
+                        if (event.target.matches(child + ', ' + child + ' *')) {
+                            _cb.apply(event.target.closest(child), arguments);
                         }
                     }, false);
                 });
             });
         }
         else {
-            callback = child;
-            this.each(function(element) {
-                array.forEach((event) => {
+            _cb = child;
+            this.each(function(e) {
+                arr.forEach((event) => {
                     event = event.trim();
                     if (state)
-                        element.addEventListener(event, callback, state);
+                        e.addEventListener(event, _cb, state);
                     else
-                        element.addEventListener(event, callback, false);
+                        e.addEventListener(event, _cb, false);
                 });
             });
         }
         return this;
     }
-    disable(element){
-        return this[0].disabled=element;
+    disable(arg){
+        return this[0].disabled=arg;
     }
 }
-const $ = function(element) {
-    return new JBase(element);
+const $ = function(e) {
+    return new JBase(e);
 }
-$.ajax=function(argument) {
-    let url = argument["url"];
+$.ajax=function(arg) {
+    let url = arg["url"];
     let type = "get";
     let success =function(){};
     let fail = function(){};
-    if(argument['success']){
-        success=argument['success'];
+    if(arg['success']){
+        success = arg['success'];
     }
-    if(argument['fail']){
-        fail=argument['fail'];
+    if(arg['fail']){
+        fail = arg['fail'];
     }
     let xhttp = new XMLHttpRequest();
     xhttp.onerror = function(error){
@@ -221,20 +219,20 @@ $.ajax=function(argument) {
         else
             return fail(this.status);
     };
-    let parameters="";
-    if (argument) {
-        type = argument["type"];
-        if ('data' in argument) {
-            parameters = new URLSearchParams(argument['data']).toString();
+    let param="";
+    if (arg) {
+        type = arg["type"];
+        if ('data' in arg) {
+            param = new URLSearchParams(arg['data']).toString();
         }
     }
     if (type && type.toUpperCase()=='POST') {
         xhttp.open("POST", url, true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send(parameters);
+        xhttp.send(param);
     }
     else if (!type || type.toUpperCase()=='GET'){
-        xhttp.open("GET", url + "?" + parameters, true);
+        xhttp.open("GET", url + "?" + param, true);
         xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xhttp.send();
     }
